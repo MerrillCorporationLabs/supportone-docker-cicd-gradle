fbe13b5f1e02ab8ccb1eb53ebdd3fd820f91aa7f
jross <jordan.ross@merrillcorp.com>
2019-01-11 09:07:30 -0600

    APPS-238 updating docker build and run commands

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ curDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker_version=$(cat VERSION)
 
-docker build --no-cache --pull -t mrllsvc/supportone-docker-cicd-service:"${docker_version}-gradle" .
+docker build --no-cache --pull -t merrillcorporation/supportone-docker-cicd-gradle:"${docker_version}" .
 
 docker run \
 		-d -it --rm -p 3000:3000 \
 		--name supportone-docker-cicd-gradle \
-		mrllsvc/supportone-docker-cicd-gradle:"${docker_version}-gradle"
+		merrillcorporation/supportone-docker-cicd-gradle:"${docker_version}"
